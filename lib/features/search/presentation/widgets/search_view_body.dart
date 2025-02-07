f60533dc8c5b122d08f30a4c8b56aa81b9bcfb9b
AhmedHossamEissa1 <ahmedhossameissa65@gmail.com>
2025-02-07 15:45:23 +0200

    finish search view

--- lib/features/search/presentation/widgets/search_view_body.dart
+++ lib/features/search/presentation/widgets/search_view_body.dart
@@ -1,4 +1,7 @@
+import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
+import 'package:bookly_app/features/home/presentation/widgets/best_sellar_list_view.dart';
 import 'package:bookly_app/features/search/presentation/widgets/cutom_search_text_field.dart';
+import 'package:bookly_app/styles.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -7,14 +10,30 @@ class SearchViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 30,
-          ),
-          CutomSearchTextField(),
-        ],
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            CutomSearchTextField(),
+            SizedBox(
+              height: 10,
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Results",
+                style: Styles.textStyle22,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            BestSellarListView()
+          ],
+        ),
       ),
     );
   }
