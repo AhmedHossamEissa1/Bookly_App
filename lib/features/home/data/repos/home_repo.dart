09522f933dfcc03ed2bce62ff74be1c24d953ce9
AhmedHossamEissa1 <ahmedhossameissa65@gmail.com>
2025-02-08 19:20:33 +0200

    finish home repo

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/core/utils/errors/failure.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellarBooks();
-  fetchFeaturesBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellarBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturesBooks();
 }
