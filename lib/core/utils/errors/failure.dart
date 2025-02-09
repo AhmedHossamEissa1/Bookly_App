e25b6172b37b87fd4366b1c20ca4276cb6d31559
AhmedHossamEissa1 <ahmedhossameissa65@gmail.com>
2025-02-09 17:25:25 +0200

    finish server failure

--- lib/core/utils/errors/failure.dart
+++ lib/core/utils/errors/failure.dart
@@ -1,3 +1,37 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errorMessage;
+
+  Failure(this.errorMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errorMessage);
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('connectionTimeout');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('sendTimeout');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('receiveTimeout');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('badCertificate');
+      case DioExceptionType.badResponse:
+        return ServerFailure('badResponse');
+      case DioExceptionType.cancel:
+        return ServerFailure('cancel');
+      case DioExceptionType.connectionError:
+        return ServerFailure('connectionError');
+      case DioExceptionType.unknown:
+        return ServerFailure('unknown');
+        
+      default: return ServerFailure('unknown');
+    }
+  }
+}
+
+
+
