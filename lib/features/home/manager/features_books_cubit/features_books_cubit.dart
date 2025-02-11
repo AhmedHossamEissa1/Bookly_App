867f43a20a29c98ff6d78c87af939045aaac4238
AhmedHossamEissa1 <ahmedhossameissa65@gmail.com>
2025-02-11 15:14:04 +0200

    creae features method

---
+++ lib/features/home/manager/features_books_cubit/features_books_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:bookly_app/features/home/manager/features_books_cubit/featues_books_states.dart';
+
+class FeaturesBooksCubit extends Cubit<FeatursBooksStates> {
+  FeaturesBooksCubit(this.homeRepo) : super(FeatursBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturesBooks() async {
+    emit(FeatursBooksLoading());
+    var result = await homeRepo.fetchFeaturesBooks();
+
+    result.fold((left) {
+      emit(FeatursBooksFail(left.errorMessage));
+    }, (right) {
+      emit(FeatursBooksSuccess(right));
+    });
+  }
+}
