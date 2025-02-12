9cd6eb045b77f9a760bd55ecf0d27795b0796c71
AhmedHossamEissa1 <ahmedhossameissa65@gmail.com>
2025-02-12 13:56:03 +0200

    use cached network image to handle error images

--- lib/features/home/presentation/widgets/features_list_view_item.dart
+++ lib/features/home/presentation/widgets/features_list_view_item.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class FeaturesListViewItem extends StatelessWidget {
@@ -7,15 +8,15 @@ class FeaturesListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.8 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            image: DecorationImage(
-              image: NetworkImage(imgUrl),
-              fit: BoxFit.fill,
-            )),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imgUrl,
+          errorWidget: (context, url, error) => Icon(Icons.error),
+        ),
       ),
     );
   }
